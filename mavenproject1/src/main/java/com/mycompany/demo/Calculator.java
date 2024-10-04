2b86d67a9617dd21aa45b7d255bee8b41065577f
Mariana Santos <marianasferra31@gmail.com>
2024-10-04 11:19:14 -0300

    feat: nova calculadora

---
+++ mavenproject1/src/main/java/com/mycompany/demo/Calculator.java
@@ -0,0 +1,49 @@
+package com.mycompany.demo;
+
+import static com.mycompany.demo.Calculator.soma;
+import javax.swing.JOptionPane;
+
+public class Calculator {
+      static int op = Integer.parseInt(JOptionPane.showInputDialog("Insira a opçao desejada: \n 1-Soma \n 2-Subtração \n 3-Multiplicação \n 4-Divisão"));
+      static double  num1 =  Double.parseDouble(JOptionPane.showInputDialog("Insira o primeiro numero"));
+      static double  num2 =  Double.parseDouble(JOptionPane.showInputDialog("Insira o segundo numero"));
+    public static void main(String[] args) {
+      if (op == 1){
+        soma(num1, num2);
+      } else if (op == 2) {
+        subtracao(num1, num2);
+      } else if (op == 3) {
+        multiplicacao(num1, num2);
+      } else{
+        divisao(num1, num2);
+      }
+    }
+    
+    public static double soma(double num1, double num2) {
+       double response = num1 + num2;
+       System.out.println("A soma dos dois números é " + response);
+       return response; 
+    }
+    
+    public static double subtracao(double num1, double num2){
+        double response = num1 - num2;
+        System.out.println("A subtração dos dois números é " + response);
+        return response;
+    }
+    
+    public static double multiplicacao(double num1, double num2){
+        double response = num1 * num2;
+        System.out.println("A multiplicação dos dois números é " + response);
+        return response;
+    }
+    
+    public static double divisao(double num1, double num2){
+        double response = num1 / num2;
+        System.out.println("A divisão dos dois números é " + response);
+        return response;
+    }
+    
+    
+    
+    
+}
